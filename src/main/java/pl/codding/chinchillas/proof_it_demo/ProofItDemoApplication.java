dadb7e4ec7f8d7a110c4f7d54eff5fcb2c761458
Godziątkowski Jan <godziatkowski.jan@gmail.com>
2022-03-14 23:52:07 +0100

    Setup project

---
+++ src/main/java/pl/codding/chinchillas/proof_it_demo/ProofItDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.codding.chinchillas.proof_it_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProofItDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProofItDemoApplication.class, args);
+	}
+
+}
